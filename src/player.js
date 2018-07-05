641d27d23a06a67f135a07e5e54009e4ccee54c5
AlexFuro <afurones43@gmail.com>
2018-07-05 12:34:45 -0400

    Add player factory

---
+++ src/player.js
@@ -0,0 +1,22 @@
+const player = (name) => {
+  const moves = [];
+  const attack = (coor) => {
+    if (coor[0] < 0 || coor[0] > 9 || coor[1] < 0 || coor[1] > 9) {
+      return false;
+    }
+    if (moves.some(move => move[0] === coor[0]
+        && move[1] === coor[1])) {
+      return false;
+    }
+    moves.push(coor);
+    return coor;
+  };
+  const randomMove = () => {
+    const x = Math.floor(Math.random(10) * 10);
+    const y = Math.floor(Math.random(10) * 10);
+    return attack([x, y]);
+  };
+  return { name, attack, randomMove };
+};
+
+module.exports = player;
---
+++ src/player.test.js
@@ -0,0 +1,27 @@
+const player = require('./player');
+
+test('players can have a name', () => {
+  const player1 = player('player 1');
+  expect(player1.name).toEqual('player 1');
+});
+
+test('players can make a atttack', () => {
+  const player1 = player('p1');
+  expect(player1.attack([1, 2])).toBeTruthy();
+});
+
+test('players cannot make an invalid move', () => {
+  const player1 = player('p1');
+  expect(player1.attack([-1, 2])).toBeFalsy();
+});
+
+test('players cannot make the same attack more than once', () => {
+  const player1 = player('p1');
+  expect(player1.attack([1, 2])).toBeTruthy();
+  expect(player1.attack([1, 2])).toBeFalsy();
+});
+
+test('player can generate a valid random attack', () => {
+  const player2 = player('computer');
+  expect(player2.randomMove()).toBeTruthy();
+})

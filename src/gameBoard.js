31847e15c8ddbd48b9a150c4a463dce3c71c943c
AlexFuro <afurones43@gmail.com>
2018-07-05 11:49:14 -0400

    Add gameboard factory

---
+++ src/gameBoard.js
@@ -0,0 +1,59 @@
+const gameBoard = () => {
+  const fleet = [];
+  const misses = [];
+
+  const shipMatch = (coor) => {
+    let match = [];
+    for (let index = 0; index < fleet.length; index += 1) {
+      const member = fleet[index];
+      for (let i = 0; i < member.ship.length; i += 1) {
+        if (member.rotate && coor[0] === member.pos.x
+            && coor[1] === member.pos.y + i) {
+          match = [index, i];
+        } else if (coor[0] === member.pos.x + i
+            && coor[1] === member.pos.y) {
+          match = [index, i];
+        }
+      }
+    }
+    return match;
+  };
+  const place = (coor, ship, rotate = false) => {
+    if (coor[0] > 9 || coor[0] < 0 || coor[1] > 9 || coor[1] < 0) {
+      return false;
+    }
+    if ((!rotate && coor[0] + ship.length > 9)
+        || (rotate && coor[1] + ship.length > 9)) {
+      return false;
+    }
+    if (shipMatch(coor).length === 0) {
+      const newFleetMem = {};
+      newFleetMem.pos = {
+        x: coor[0],
+        y: coor[1],
+        rotate,
+      };
+      newFleetMem.ship = ship;
+      fleet.push(newFleetMem);
+      return true;
+    }
+    return false;
+  };
+  const receiveAttack = (coor) => {
+    const strike = shipMatch(coor);
+    if (strike.length !== 0) {
+      const shipId = strike[0];
+      const hitBox = strike[1];
+      fleet[shipId].ship.hit(hitBox);
+    } else {
+      misses.push(coor);
+    }
+    return true;
+  };
+  const sunkFleet = () => fleet.every(member => member.ship.isSunk() === true);
+  return {
+    misses, place, receiveAttack, sunkFleet,
+  };
+};
+
+module.exports = gameBoard;
---
+++ src/gameBoard.test.js
@@ -0,0 +1,71 @@
+const gameBoard = require('./gameBoard');
+const ship = require('./ship');
+
+test('can place a ship given a set of coordinates', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 1', 3);
+  expect(newBoard.place([0, 0], testShip)).toBeTruthy();
+});
+
+test('cannot place a ship outside board with invalid coordinates', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 2', 3);
+  expect(newBoard.place([2, 10], testShip)).toBeFalsy();
+});
+
+test('cannot place a ship outside board vertically', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 3', 3);
+  expect(newBoard.place([8, 0], testShip)).toBeFalsy();
+});
+
+test('can place a ship sideways on board', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 4', 3);
+  const rotate = true;
+  expect(newBoard.place([8, 0], testShip, rotate)).toBeTruthy();
+});
+
+test('cannot place a ship outside board horizontally', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 5', 3);
+  const rotate = true;
+  expect(newBoard.place([8, 8], testShip, rotate)).toBeFalsy();
+});
+
+test('cannot place a ship on top of another', () => {
+  const newBoard = gameBoard();
+  const testShip1 = ship('test 6', 3);
+  expect(newBoard.place([0, 0], testShip1)).toBeTruthy();
+  const testShip2 = ship('test 7', 2);
+  expect(newBoard.place([1, 0], testShip2)).toBeFalsy();
+});
+
+test('can receive attack coordinates and attack a ship', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 8', 3);
+  newBoard.place([0, 0], testShip);
+  expect(newBoard.receiveAttack([1, 0])).toBeTruthy();
+});
+test('can record missed attacks', () => {
+  const newBoard = gameBoard();
+  const testShip = ship('test 9', 3);
+  newBoard.place([0, 0], testShip);
+  newBoard.receiveAttack([6, 0]);
+  expect(newBoard.misses).toEqual([[6, 0]]);
+});
+
+test('can determine if a fleet has sunk', () => {
+  const newBoard = gameBoard();
+  const testShip1 = ship('test 10', 3);
+  newBoard.place([0, 0], testShip1);
+  const testShip2 = ship('test 11', 2);
+  newBoard.place([0, 1], testShip2, true);
+  //attack ships until sunk
+  newBoard.receiveAttack([0, 0]);
+  newBoard.receiveAttack([1, 0]);
+  newBoard.receiveAttack([2, 0]);
+  newBoard.receiveAttack([0, 1]);
+  newBoard.receiveAttack([1, 1]);
+  expect(newBoard.sunkFleet()).toBeTruthy();
+});

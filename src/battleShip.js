768d4c16c70d2f2642737faf0036e92dd983ff84
AlexFuro <afurones43@gmail.com>
2018-07-10 09:22:29 -0400

    Add main game logic

---
+++ src/battleShip.js
@@ -0,0 +1,66 @@
+const ship = require('./ship');
+
+const battleShip = (players, gameBoards) => {
+  const makeFleet = () => {
+    const destroyer = ship('destroyer', 2);
+    const sub = ship('submarine', 3);
+    const cruiser = ship('cruiser', 3);
+    const battle = ship('battleship', 4);
+    const carrier = ship('carrier', 5);
+    return [destroyer, sub, cruiser, battle, carrier];
+  };
+  const initPlayerBoard = (customPos = []) => {
+    const fleet = makeFleet();
+    const fleetPos = customPos;
+    if (fleetPos.length === 0) {
+      fleetPos.push({ coor: [0, 0], rotate: false });
+      fleetPos.push({ coor: [6, 0], rotate: false });
+      fleetPos.push({ coor: [0, 9], rotate: false });
+      fleetPos.push({ coor: [0, 2], rotate: true });
+      fleetPos.push({ coor: [4, 3], rotate: true });
+    }
+    for (let i = 0; i < fleet.length; i += 1) {
+      gameBoards[0].place(fleetPos[i].coor, fleet[i], fleetPos[i].rotate);
+    }
+  };
+  const randomPlacement = () => {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    const sideWays = Math.floor(Math.random() * 2);
+    if (sideWays === 0) {
+      return { coor: [x, y], rotate: false };
+    }
+    return { coor: [x, y], rotate: true };
+  };
+  const initCompBoard = () => {
+    const fleet = makeFleet();
+    let shipPos;
+    let placed;
+    for (let i = 0; i < fleet.length; i += 1) {
+      do {
+        shipPos = randomPlacement();
+        placed = gameBoards[1].place(shipPos.coor, fleet[i], shipPos.rotate);
+      } while (!placed);
+    }
+  };
+  const gameOver = () => {
+    if (gameBoards[0].sunkFleet() || gameBoards[1].sunkFleet()) {
+      return true;
+    }
+    return false;
+  };
+  const winner = () => {
+    if (gameBoards[0].sunkFleet()) {
+      return players[1].name;
+    }
+    if (gameBoards[1].sunkFleet()) {
+      return players[0].name;
+    }
+    return false;
+  };
+  return {
+    players, gameBoards, initPlayerBoard, initCompBoard, gameOver, winner,
+  };
+};
+
+module.exports = battleShip;
---
+++ src/battleship.test.js
@@ -0,0 +1,89 @@
+const battleShip = require('./battleShip');
+const gameBoard = require('./gameBoard');
+
+test('has two players', () => {
+  const game = battleShip(['player', 'computer']);
+  expect(game.players.length).toEqual(2);
+});
+
+test('has two boards', () => {
+  const players = ['player', 'computer'];
+  const gameBoards = ['board 1', 'board 2'];
+  const game = battleShip(players, gameBoards);
+  expect(game.gameBoards.length).toEqual(2);
+});
+
+test('places player fleet', () => {
+  const players = ['player', 'computer'];
+  const gameBoards = [gameBoard(), gameBoard()];
+  const game = battleShip(players, gameBoards);
+  game.initPlayerBoard();
+  expect(game.gameBoards[0].fleet[0].pos).toEqual({ x: 0, y: 0, rotate: false });
+  expect(game.gameBoards[0].fleet[1].pos).toEqual({ x: 6, y: 0, rotate: false });
+  expect(game.gameBoards[0].fleet[2].pos).toEqual({ x: 0, y: 9, rotate: false });
+  expect(game.gameBoards[0].fleet[3].pos).toEqual({ x: 0, y: 2, rotate: true });
+  expect(game.gameBoards[0].fleet[4].pos).toEqual({ x: 4, y: 3, rotate: true });
+});
+
+test('places player fleet with custom coordinates', () => {
+  const players = ['player', 'computer'];
+  const gameBoards = [gameBoard(), gameBoard()];
+  const game = battleShip(players, gameBoards);
+  const fleetPos = [
+    { coor: [9, 0], rotate: true },
+    { coor: [7, 3], rotate: false },
+    { coor: [6, 5], rotate: true },
+    { coor: [4, 3], rotate: true },
+    { coor: [1, 1], rotate: false }
+  ];
+  game.initPlayerBoard(fleetPos);
+  expect(game.gameBoards[0].fleet[0].pos).toEqual({ x: 9, y: 0, rotate: true });
+  expect(game.gameBoards[0].fleet[1].pos).toEqual({ x: 7, y: 3, rotate: false });
+  expect(game.gameBoards[0].fleet[2].pos).toEqual({ x: 6, y: 5, rotate: true });
+  expect(game.gameBoards[0].fleet[3].pos).toEqual({ x: 4, y: 3, rotate: true });
+  expect(game.gameBoards[0].fleet[4].pos).toEqual({ x: 1, y: 1, rotate: false });
+});
+
+test(('places comp fleet'), () => {
+  const players = ['player', 'computer'];
+  const gameBoards = [gameBoard(), gameBoard()];
+  const game = battleShip(players, gameBoards);
+  game.initCompBoard();
+  expect(game.gameBoards[1].fleet.length).toEqual(5);
+});
+
+test('can tell if a game is ongoing', () => {
+  const players = ['player', 'computer'];
+  const gameBoards = [gameBoard(), gameBoard()];
+  const game = battleShip(players, gameBoards);
+  game.initPlayerBoard();
+  game.initCompBoard();
+  expect(game.gameOver()).toBeFalsy();
+});
+
+test('determine a winner', () => {
+  const players = [{ name: 'player' }, { name: 'computer' }];
+  const gameBoards = [gameBoard(), gameBoard()];
+  const game = battleShip(players, gameBoards);
+  game.initPlayerBoard();
+  game.initCompBoard();
+  game.gameBoards[0].receiveAttack([0, 0]);
+  game.gameBoards[0].receiveAttack([1, 0]);
+  game.gameBoards[0].receiveAttack([6, 0]);
+  game.gameBoards[0].receiveAttack([7, 0]);
+  game.gameBoards[0].receiveAttack([8, 0]);
+  game.gameBoards[0].receiveAttack([0, 9]);
+  game.gameBoards[0].receiveAttack([1, 9]);
+  game.gameBoards[0].receiveAttack([2, 9]);
+  game.gameBoards[0].receiveAttack([0, 2]);
+  game.gameBoards[0].receiveAttack([0, 3]);
+  game.gameBoards[0].receiveAttack([0, 4]);
+  game.gameBoards[0].receiveAttack([0, 5]);
+  game.gameBoards[0].receiveAttack([4, 3]);
+  game.gameBoards[0].receiveAttack([4, 4]);
+  game.gameBoards[0].receiveAttack([4, 5]);
+  game.gameBoards[0].receiveAttack([4, 6]);
+  game.gameBoards[0].receiveAttack([4, 7]);
+  expect(game.gameOver()).toBeTruthy();
+  expect(game.winner()).toBe('computer');
+});
